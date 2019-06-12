41272bdf27b6a6e9df096103754734c0ed9e1ed8
Vanderlei Vinicius <vanderlei_vinicius@hotmail.com>
2019-06-12 20:54:05 -0300

    alterando rota

--- config/config-db.js
+++ config/config-db.js
@@ -1,8 +1,8 @@
 "use strict";
 
  var configdb = {
-
-     "stringConnection": "mongodb://localhost:27017/meriti-money"
+  //  "stringConnection": "mongodb://db-merit-money:pXW4FpLoOmVD7O0iYZ0byCa675hYlMcrt4u98Jnx4LZoUgEmgdA237GAczHkAh8mbIJnhj83gGmfOXCJX8UD2g==@bd-meriti-money.documents.azure.com:10255/meriti-money?ssl=true&replicaSet=globaldb"
+    "stringConnection": "mongodb://db-merit-money:6HLnGIRT8HuSb5zymTaooNGgq9PVUHdg57lA7gyn6l3H56vcT50KkhpdmiLylym3XVMWzPVfeSYuNUYN0uWUUw==@db-merit-money.documents.azure.com:10255/?ssl=true&replicaSet=globaldb";
  };
    // "stringConnection": "mongodb://bd-meriti-money:pXW4FpLoOmVD7O0iYZ0byCa675hYlMcrt4u98Jnx4LZoUgEmgdA237GAczHkAh8mbIJnhj83gGmfOXCJX8UD2g==@bd-meriti-money.documents.azure.com:10255/meriti-money?ssl=true&replicaSet=globaldb"
 
